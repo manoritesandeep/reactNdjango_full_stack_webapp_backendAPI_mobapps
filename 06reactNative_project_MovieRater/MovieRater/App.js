89cde4e5fbf7087c40e08f4deeafdd9b659b2235
Sandeep Solanki <manoritesandeep@gmail.com>
2024-03-03 05:54:04 -0600

    React native project app initial commit... added list.js file as setup file to hold movielist details

---
+++ 06reactNative_project_MovieRater/MovieRater/App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MovieList from './components/list';
+
+export default function App() {
+  return (
+    <MovieList />
+  );
+}
